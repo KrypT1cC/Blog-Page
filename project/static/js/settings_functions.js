4ab411f1917cf1b1d5fc408b67e94d891454c592
KrypT1cC <68301003+KrypT1cC@users.noreply.github.com>
2021-04-04 10:53:57 -0700

    Dropdown Button Form Feature
    added feature to change forms on click on dropdown buttons using js and css

---
+++ project/static/js/settings_functions.js
@@ -0,0 +1,32 @@
+var array = ["change-username", "change-password", "change-email", "change-profile-pic"];
+
+function swapHide(className){
+    showClass(className);
+    for (var i = 0; i < array.length; i++)
+    {
+        if (array[i] != className)
+        {
+            hideClass(array[i]);
+        }
+    }
+
+}
+
+function showClass(className)
+{
+    var formItems = [...document.getElementsByClassName(className)];
+    for (var i = 0; i < formItems.length; i++)
+    {
+        formItems[i].style.display = "block";
+    }
+}
+
+function hideClass(className){
+    var allItems = [...document.getElementsByClassName(className)];
+    for (var i = 0; i < allItems.length; i++)
+    {
+        allItems[i].style.display = "none";
+    }
+}
+
+
